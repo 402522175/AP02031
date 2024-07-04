9b2d80ac9819d141cbc589ae9766767aa15602ba
matin <matinnouri0938@gmail,com>
2024-07-04 22:55:48 +0330

    13

---
+++ s13/s13.con/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+
+class Autostopwatch : IDisposable
+{
+    private Stopwatch m_task;
+
+    public Autostopwatch(string task)
+    {
+        Console.WriteLine($"Start: {task}");
+        m_task = Stopwatch.StartNew();
+    }
+
+    public void Dispose()
+    {
+        m_task.Stop();
+        Console.WriteLine($"Task completed in {m_task.Elapsed}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (Autostopwatch asw = new Autostopwatch("task"))
+        {
+            for(int i = 0; i <= 10 ; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
